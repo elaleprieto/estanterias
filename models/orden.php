254fa3037926be190ae2d7bd1871215762f8a277
Ale Prieto <aleprieto@gmail.com>
2012-03-12 16:51:10 -0300

    Definición de Campos Auxiliares.

--- models/orden.php
+++ models/orden.php
@@ -3,7 +3,11 @@ class Orden extends AppModel {
 	var $name = 'Orden';
 	var $virtualFields = array(
 		// 'precio_venta' => 'SELECT COUNT(*) FROM Ordenes AS ordenes WHERE ordenes.pedido_id = Pedido.id GROUP BY ordenes.pedido_id',
+		'articulo_orden' => 'SELECT orden FROM Articulos as articulos WHERE articulos.id = Orden.articulo_id',
 		'articulo_detalle' => 'SELECT detalle FROM Articulos as articulos WHERE articulos.id = Orden.articulo_id',
+		'articulo_unidad' => 'SELECT unidad FROM Articulos as articulos WHERE articulos.id = Orden.articulo_id',
+		'articulo_foto' => 'SELECT foto FROM Articulos as articulos WHERE articulos.id = Orden.articulo_id',
+		'articulo_precio_venta' => 'SELECT (precio + precio * porcentaje / 100) FROM Articulos as articulos WHERE articulos.id = Orden.articulo_id',
 	);
 	var $validate = array(
 		//'estado' => array(
@@ -45,11 +49,18 @@ class Orden extends AppModel {
 			'foreignKey' => 'articulo_id',
 			'conditions' => '',
 			'fields' => '',
-			'order' => ''
+			'order' => 'orden ASC'
 		),
 		'Pedido' => array(
 			'className' => 'Pedido',
 			'foreignKey' => 'pedido_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		),
+		'Bulto' => array(
+			'className' => 'Bulto',
+			'foreignKey' => 'bulto_id',
 			'conditions' => '',
 			'fields' => '',
 			'order' => ''
